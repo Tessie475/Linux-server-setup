765b2cc7dedddf7f0dc31d2db7d745214136c22c
Tessie475 <eberechichukwu1@gmail.com>
2023-10-17 10:48:04 +0100

    added a security group, a route table and an internet gateway

--- main.tf
+++ main.tf
@@ -24,8 +24,53 @@ resource "aws_subnet" "my_project_subnet" {
   availability_zone = "us-east-1a"
 }
 
-# Create an EC2 instance within the subnet
+# Create an internet gateway
+resource "aws_internet_gateway" "my-project-igw" {
+    vpc_id = aws_vpc.my_project_vpc.id
+    tags = {
+        Name: "my_project-ig"
+    }
+}
 
+# Create a route table
+resource "aws_default_route_table" "main-rtb" {
+    default_route_table_id = aws_vpc.my_project_vpc.default_route_table_id 
+
+     route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.my-project-igw.id
+    }
+    tags = {
+        Name: "my-project-main-rtb"
+    }
+}
+
+# Configure instance to allow ingress traffic 
+resource "aws_security_group" "my-project-sg" {
+  name        = "my-project-sg"
+  description = "Allows ingress traffic on port 22"
+  vpc_id      = aws_vpc.my_project_vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+    tags = {
+    Name = "allow_all_ssh"
+  }
+}
+
+# Create an EC2 instance within the subnet
 data "aws_ami" "latest-amazon-linux-image" {
     most_recent = true
     owners = ["amazon"]
@@ -46,7 +91,10 @@ resource "aws_instance" "my_project_server" {
   ami = data.aws_ami.latest-amazon-linux-image.id
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.my_project_subnet.id
+  vpc_security_group_ids = [aws_security_group.my-project-sg.id]
+  availability_zone = "us-east-1a"
 
   associate_public_ip_address = true
-  key_name = "my-project"
+  key_name = "linux-setup-project"
 }
+
